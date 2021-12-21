22dee30ed5de3fbff61ac2526e3af392ed78e3e0
MaiQuangHiep97 <74808331+MaiQuangHiep97@users.noreply.github.com>
2021-12-22 00:32:48 +0700

    Feat add cart use session

--- app/controllers/customer/CartController.php
+++ app/controllers/customer/CartController.php
@@ -3,14 +3,36 @@ class CartController extends Controller
 {
     public $model;
     public $data = array();
-    public function __construct()
-    {
-        $this->model = $this->model('ProductModel');
-    }
     public function index()
     {
     }
     public function add()
     {
+        $id = $_POST['product_id'];
+        if (isset($_SESSION['cart'])&&array_key_exists($id, $_SESSION['cart']['buy'])) {
+            $qty = $_SESSION['cart']['buy'][$id]['qty'] + $_POST['product_qty'];
+            $_SESSION['cart']['buy'][$id] = array(
+                'product_id' => $_POST['product_id'],
+                'product_name' => $_POST['product_name'],
+                'product_price' => $_POST['product_price'],
+                'product_thumb' => $_POST['product_thumb'],
+                'qty' => $qty,
+                'sub_total'=> $_POST['product_price']*$qty
+                                
+            );
+        } else {
+            $_SESSION['cart']['buy'][$id] = array(
+                'product_id' => $_POST['product_id'],
+                'product_name' => $_POST['product_name'],
+                'product_price' => $_POST['product_price'],
+                'product_thumb' => $_POST['product_thumb'],
+                'qty' => $_POST['product_qty'],
+                'sub_total'=> $_POST['product_price']*$_POST['product_qty']
+                                
+            );
+        }
+        echo '<pre>';
+        print_r($_SESSION['cart']['buy']);
+        //session_destroy();
     }
 }
